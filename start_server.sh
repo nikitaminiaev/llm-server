681dc82c4ee96473b313443b447281f71c704fd9
nikita <you@example.com>
2026-05-22 00:47:25 +0400

    plus models

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-llama-server --models-preset ~/models/confog/models.ini --host 0.0.0.0 --port 8080 --models-max 1 --parallel 1
+llama-server --models-preset ~/models/config/models.ini --host 0.0.0.0 --port 8080 --models-max 3 --parallel 1
 
